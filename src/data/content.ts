c7c5e3cf00ee546bf9d7c6df2a8ef5bf7a5bb607
shreyasgoud <shreyasgoud5148@gmail.com>
2025-01-26 16:06:55 +0530

    portfolio created

---
+++ src/data/content.ts
@@ -0,0 +1,64 @@
+export const experiences = [
+  {
+    id: "1",
+    company: "NG-RAN IIT Hyderabad",
+    position: "Project Intern",
+    period: "2023-2024",
+    description: [
+      "I completed a five-month project internship at Future Wireless Communication, where I worked on developing innovative solutions for wireless communication.",
+      "I collaborated with a team of researchers and engineers, and contributed to the design, implementation, and testing of a prototype system that improved the performance and reliability of wireless networks.",
+    ],
+  },
+];
+
+export const projects = [
+  // {
+  //   id: "1",
+  //   title: "E-commerce Platform",
+  //   description: "A modern e-commerce platform built with Next.js and Stripe",
+  //   technologies: ["React", "Next.js", "TypeScript", "Stripe", "Tailwind CSS"],
+  //   link: "https://github.com/yourusername/ecommerce",
+  // },
+  // {
+  //   id: "2",
+  //   title: "Task Management System",
+  //   description: "Real-time task management app with collaborative features",
+  //   technologies: ["React", "Node.js", "Socket.io", "PostgreSQL"],
+  //   link: "https://github.com/yourusername/task-manager",
+  // },
+  // {
+  //   id: "2",
+  //   title: "Task Management System",
+  //   description: "Real-time task management app with collaborative features",
+  //   technologies: ["React", "Node.js", "Socket.io", "PostgreSQL"],
+  //   link: "https://github.com/yourusername/task-manager",
+  // },
+  // {
+  //   id: "2",
+  //   title: "Task Management System",
+  //   description: "Real-time task management app with collaborative features",
+  //   technologies: ["React", "Node.js", "Socket.io", "PostgreSQL"],
+  //   link: "https://github.com/yourusername/task-manager",
+  // },
+  // {
+  //   id: "2",
+  //   title: "Task Management System",
+  //   description: "Real-time task management app with collaborative features",
+  //   technologies: ["React", "Node.js", "Socket.io", "PostgreSQL"],
+  //   link: "https://github.com/yourusername/task-manager",
+  // },
+  // {
+  //   id: "2",
+  //   title: "Task Management System",
+  //   description: "Real-time task management app with collaborative features",
+  //   technologies: ["React", "Node.js", "Socket.io", "PostgreSQL"],
+  //   link: "https://github.com/yourusername/task-manager",
+  // },
+];
+
+export const skills = {
+  languages: ["TypeScript", "JavaScript", "Python", "SQL"],
+  frontend: ["React", "Tailwind CSS"],
+  backend: ["Node.js", "Express", "MongoDB"],
+  tools: ["Git"],
+};
